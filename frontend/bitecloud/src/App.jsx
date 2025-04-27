c909c88b96e308128be959082ad6cb336adcb3f6
Dhiyanah <dheeliyaudeen@gmail.com>
2025-04-27 20:45:43 +0530

    app.jsx - incoming change accepted

--- frontend/bitecloud/src/App.jsx
+++ frontend/bitecloud/src/App.jsx
@@ -62,24 +62,14 @@ function App() {
           <Route path="/admin/edit-offer/:id" element={<EditOfferForm />} />
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
         </Route>
+        
           {/* Cart and Checkout */}
         <Route path="/cart" element={<CartPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/delivery-tracker/:orderId" element={<DeliveryTrackerPage />} />
         <Route path="/order-history" element={<OrderHistoryPage />} />
 
-<<<<<<< HEAD
-
-        {/* Dashboards for restaurant admin role */}
-        <Route path="/restaurant/dashboard" element={<RestaurantAdminDashboard />} />
-        <Route path="/restaurant/dashboard/:restaurantId" element={<RestaurantDetailDashboard />} />
-
-        <Route path = "/payment/checkout" element = {<StripePayment/>}/>
-        
-        <Route path="/notification/viewNotification" element = {<NotificationComponent/>}/>
-=======
         {/*Dashboards for driver role */}
->>>>>>> 22ff78cf466c5322649d0abc62c7c1d1363f6e6c
       </Routes>
       <ToastContainer autoClose={1500} />
     </Router>
